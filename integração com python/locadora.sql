8cf972172b8d7c1f089cd0f9db82ac34fe6166de
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-12-05 10:39:17 -0400

    Create locadora.sql

---
+++ integração com python/locadora.sql
@@ -0,0 +1,78 @@
+/*CREATE table carros(
+	id_carro INTEGER unique PRIMARY KEY AUTOINCREMENT,
+	marca TEXT,
+	modelo TEXT,
+	cor TEXT,
+	placa TEXT ,
+	ano INTEGER,
+	preco_dia INTEGER
+);
+
+CREATE table pessoa(
+	id_pessoa INTEGER unique PRIMARY KEY AUTOINCREMENT,
+	nome TEXT,
+	idade INTEGER,
+	cpf INTEGER,
+	cnh INTEGER
+);
+
+CREATE table aluga(
+	id_aluguel INTEGER unique PRIMARY KEY AUTOINCREMENT,
+	data_alugada TEXT,
+	data_devoluçao TEXT,
+	preco_aluguel INTEGER,
+	id_carro INTEGER,
+	id_pessoa INTEGER,
+	FOREIGN key(id_pessoa) REFERENCES pessoa(id_pessoa),
+	FOREIGN key(id_carro) REFERENCES carros(id_carro)
+);
+
+insert into carros (marca, modelo, ano, cor, preco_dia, placa)
+	VALUES("Toyota", "Camry", 2022, "Prata", 80.0, "ABC-1234"),
+		("Honda", "Civic", 2021, "Azul", 95.0, "XYZ-5678"),
+		("Ford", "Escape", 2023, "Preto", 90.0, "KLM-9876"),
+		("Chevrolet", "Trax", 2022, "Branco", 85.0, "QRS-4321"),
+		("Nissan", "Versa", 2021, "Vermelho", 70.0,  "DEF-6543"),
+		("Hyundai", "Accent", 2022, "Prata", 65.0,  "GHI-7890"),
+		("Renault", "Kwid", 2017, "Prata", 50.0,  "MNO-2109"),
+		("Volkswagen", "Golf", 2023, "Azul", 88.0, "PQR-1111"),
+		("Mazda", "Mazda3", 2022, "Cinza", 75.0,  "STU-2222"),
+		("Kia", "Forte", 2023, "Vermelho", 82.0,  "VWX-3333")
+		
+select *from carros
+insert into pessoa (nome, idade, cpf, cnh)
+	VALUES("Maria", 22 , 01802625900 , 11557786927),
+		("João", 30, 12345678901, 9876543210),
+		("Ana", 25, 98765432109, 9988776655),
+		("Carlos", 28, 11223344556, 5544332211),
+		("Larissa", 35, 87654321098, 1122334455),
+		("Pedro", 40, 55544433322, 9998887776),
+		("Isabel", 27, 99988877766, 2233445566),
+		("Rafael", 33, 65432109876, 777888999),
+		("Camila", 29, 32109876543, 4445556667),
+		("Gabriel", 22 , 08792569348 , 08970059011)
+
+SELECT * from pessoa
+
+INSERT into aluga(data_alugada, data_devoluçao, preco_aluguel, id_carro, id_pessoa)
+	VALUES("05/12/2023", "15/12/2023", 800.0, 1 , 1),
+			("10/12/2023", "20/12/2023", 950.0, 2, 2),
+			("08/12/2023", "18/12/2023", 900.0, 3, 3),
+			("12/12/2023", "22/12/2023", 850.0, 4, 4),
+			("07/12/2023", "17/12/2023", 700.0, 5, 5),
+			("06/12/2023", "16/12/2023", 650.0, 6, 6),
+			("09/12/2023", "19/12/2023", 500.0, 7, 7),
+			("11/12/2023", "21/12/2023", 880.0, 8, 8),
+			("13/12/2023", "23/12/2023", 750.0, 9, 9),
+			("14/12/2023", "24/12/2023", 820.0, 10, 10)
+
+SELECT *from aluga*/
+--Relatório da Locação do carro
+select pessoa.nome, pessoa.cpf, pessoa.cnh, carros.modelo, aluga.data_alugada, aluga.data_devoluçao, aluga.preco_aluguel
+
+FROM  aluga 
+JOIN pessoa on aluga.id_pessoa = pessoa.id_pessoa
+JOIN carros on aluga.id_carro = carros.id_carro;
+
+
+		
