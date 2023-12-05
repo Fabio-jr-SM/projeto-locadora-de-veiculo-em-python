f8146b45208e7778721e4f4cd34b7a48c3cc51fc
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-12-05 10:39:36 -0400

    Update main.py

--- integração com python/main.py
+++ integração com python/main.py
@@ -1,20 +1,49 @@
-import sqlite3 as sql
-
-con = sql.connect("banco.db")
-cur = con.cursor()
+import sqlite3
 
 
-numero = input("Digite o numero: ")
-conta = input("Digite a conta: ")
+def valor_aluguel():
+    ent = input(int('Digite o valor do aluguel do carros: '))
+    ent1 = input(int('Digite a quantidade de dias que deseja: '))
+    aluguel = ent * ent1
+    return aluguel
 
-cur.execute(f"""
-            insert into conta(numero,agencia,cliente_id)
-            values('{numero}','{conta}',4)
-            """)
-con.commit()
 
-res = cur.execute("select * from conta")
-for id,numero,agencia,cliente_id in (res.fetchall()):
-    print(id,numero,agencia,cliente_id)
+def aparecer_tabela():
+    con = sqlite3.connect()
+    cur = con.cursor()
+    mostrar_tabela = cur.execute('Select * from Exemplo')
+    return mostrar_tabela.fetchall()
 
-con.close()
+
+def alugar_carro():
+    con = sqlite3.connect()
+    cur = con.cursor()
+    alugar = cur.execute(
+        'update <tabela> set <coluna>=<valor> where <condição>')
+    return alugar.fetchone()
+
+
+def devolver_carro():
+    con = sqlite3.connect()
+    cur = con.cursor()
+    devolver = cur.execute(
+        'update <tabela> set <coluna>=<valor> where <condição>')
+    return devolver.fetchone()
+
+
+def menu():
+    while True:
+        aparecer_tabela()
+        op = input(
+            '\n(1) Reservar veiculo\n(2) Devolver veiculo\n(3) Finalizar\n')
+
+        if op == '1':
+            alugar_carro()
+        elif op == '2':
+            devolver_carro()
+        elif op == '3':
+            break
+
+
+if __name__ == 'main':
+    menu()
